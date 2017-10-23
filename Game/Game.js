16a894238de3065207f4ff4f8bd126565efff8bd
LydianLights <rane.fields@gmail.com>
2017-10-22 21:22:40 -0700

    Add player movement with keys

--- Game/Game.js
+++ Game/Game.js
@@ -1,18 +1,21 @@
 function Game() {
+  this.gameObjects = [];
   this.tickNumber = 0;
   this.isRunning = false;
-  this.viewport = new Viewport();
+  this.viewport = new Viewport(this.gameObjects);
   this.player = new Player();
 }
 
 // TODO: fix hacked-in player
 Game.prototype.start = function() {
   this.isRunning = true;
-  this.viewport.objects.push(this.player);
+  this.gameObjects.push(this.player);
   this.main();
 }
 
 Game.prototype.main = function() {
+  var currentTickTime = new Date().getTime();
+  var prevTickTime = currentTickTime;
   var currentTime = new Date().getTime();
   var prevTime = currentTime;
   var dT = 0;
@@ -20,25 +23,30 @@ Game.prototype.main = function() {
     currentTime = new Date().getTime();
     dT += currentTime - prevTime;
     if (dT >= 10) {
-      console.log("tick", dT);
+      prevTickTime = currentTickTime;
+      currentTickTime = new Date().getTime();
       this.tickNumber++;
       dT %= 10;
+      this.updateGameObjects();
     }
     prevTime = currentTime;
-  }.bind(this), 5);
+  }.bind(this), 1);
 }
 
-function Viewport() {
-  this.objects = [];
+Game.prototype.updateGameObjects = function() {
+  if (this.gameObjects.length > 0) {
+    this.gameObjects.forEach(function(gameObject) {
+      gameObject.update();
+    });
+  }
+}
+
+function Viewport(gameObjects) {
+  this.gameObjects = gameObjects;
   this.draw();
 }
 
-// Viewport.prototype.initialize = function() {
-//   setInterval(this.draw.bind(this), 100);
-// }
-
 Viewport.prototype.draw = function() {
-  console.log("draw");
   var canvas = document.getElementById("canvas");
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
@@ -46,10 +54,9 @@ Viewport.prototype.draw = function() {
     ctx.save();
 
     // do the rectangle
-    if(this.objects.length > 0)
-    {
+    if(this.gameObjects.length > 0) {
       ctx.fillStyle = 'rgb(200, 0, 0)';
-      ctx.fillRect(this.objects[0].position.x, this.objects[0].position.y, 100, 100);
+      ctx.fillRect(this.gameObjects[0].position.x, this.gameObjects[0].position.y, 100, 100);
     }
     ctx.restore();
     window.requestAnimationFrame(this.draw.bind(this));
@@ -63,12 +70,46 @@ function Player() {
     up: false,
     down: false
   };
-  this.position = new Position(50, 50);
+  this.position = new Vector(50, 50);
+  this.velocity = new Vector(0, 0);
 }
 
-function Position(x, y) {
+Player.prototype.update = function() {
+  this.getKeyInput();
+  this.position = Vector.Add(this.position, this.velocity);
+}
+
+Player.prototype.getKeyInput = function() {
+  if (this.keyDown.left === true) {
+    this.velocity.x = -3;
+  }
+  else if (this.keyDown.right === true) {
+    this.velocity.x = 3;
+  }
+  else {
+    this.velocity.x = 0;
+  }
+  if (this.keyDown.up === true) {
+    this.velocity.y = -3;
+  }
+  else if (this.keyDown.down === true) {
+    this.velocity.y = 3;
+  }
+  else {
+    this.velocity.y = 0;
+  }
+}
+
+function Vector(x, y) {
   this.x = x;
   this.y = y;
+}
+
+Vector.Add = function(Vector1, Vector2) {
+  var result = new Vector(0, 0);
+  result.x = Vector1.x + Vector2.x;
+  result.y = Vector1.y + Vector2.y;
+  return result;
 }
 
 $(document).ready(function() {
@@ -77,6 +118,32 @@ $(document).ready(function() {
 
   document.onkeydown = function(event) {
     var key = event.key;
-    if(key == "ArrowRight");
+    if (key === "ArrowRight") {
+      game.player.keyDown.right = true;
+    }
+    else if (key === "ArrowLeft") {
+      game.player.keyDown.left = true;
+    }
+    else if (key === "ArrowUp") {
+      game.player.keyDown.up = true;
+    }
+    else if (key === "ArrowDown") {
+      game.player.keyDown.down = true;
+    }
+  };
+  document.onkeyup = function(event) {
+    var key = event.key;
+    if (key === "ArrowRight") {
+      game.player.keyDown.right = false;
+    }
+    else if (key === "ArrowLeft") {
+      game.player.keyDown.left = false;
+    }
+    else if (key === "ArrowUp") {
+      game.player.keyDown.up = false;
+    }
+    else if (key === "ArrowDown") {
+      game.player.keyDown.down = false;
+    }
   };
 });
