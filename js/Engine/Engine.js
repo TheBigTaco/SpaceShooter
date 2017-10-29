6d8b0350a622ffbc99f70a8c1fe6d93051b48b6c
LydianLights <rane.fields@gmail.com>
2017-10-29 12:29:21 -0700

    Refactor engine to be separate file from game instance

---
+++ js/Engine/Engine.js
@@ -0,0 +1,151 @@
+class Game {
+  constructor(canvas) {
+    this.gameObjects = [];
+    this.isRunning = false;
+    this.tickNumber = 0;
+    this.viewport = new Viewport(canvas, this.gameObjects);
+  }
+  start() {
+    this.isRunning = true;
+    this.main();
+  }
+  main() {
+    var currentTickTime = new Date().getTime();
+    var prevTickTime = currentTickTime;
+    setInterval(function() {
+      currentTickTime = new Date().getTime();
+      this.updateGameObjects(currentTickTime - prevTickTime);
+      this.checkCollisions();
+      prevTickTime = currentTickTime;
+    }.bind(this), 10);
+  }
+  updateGameObjects(dT) {
+    if (this.gameObjects.length > 0) {
+      this.gameObjects.forEach(function(gameObject) {
+        gameObject.update(dT);
+      });
+    }
+  }
+  // TODO: be more efficient
+  checkCollisions() {
+    for (var i = 0; i < this.gameObjects.length; i++) {
+      for (var j = i + 1; j < this.gameObjects.length; j++) {
+        var collisionOverlap = this.gameObjects[i].getCollisionBoxPosition().calcIntersectionWith(this.gameObjects[j].getCollisionBoxPosition());
+        if(collisionOverlap != null) {
+          this.gameObjects[i].onCollision(new CollisionResult(this.gameObjects[j], collisionOverlap));
+          this.gameObjects[j].onCollision(new CollisionResult(this.gameObjects[i], collisionOverlap));
+        }
+      }
+    }
+  }
+}
+
+class Viewport {
+  constructor(canvas, gameObjects) {
+    this.canvas = canvas;
+    this.gameObjects = gameObjects;
+    this.drawDebugInfo = false;
+    this.draw();
+  }
+  draw() {
+    if (this.canvas.getContext) {
+      var ctx = this.canvas.getContext('2d');
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      ctx.save();
+
+      this.gameObjects.forEach(function(gameObject) {
+        // draw collision boxes
+        if(this.drawDebugInfo) {
+          ctx.fillStyle = 'rgba(0,200,0,0.3)';
+          ctx.fillRect(gameObject.getCollisionBoxPosition().x, gameObject.getCollisionBoxPosition().y, gameObject.collisionBox.width, gameObject.collisionBox.height);
+        }
+      }.bind(this));
+      ctx.restore();
+      window.requestAnimationFrame(this.draw.bind(this));
+    }
+  }
+}
+
+class GameObject {
+  constructor() {
+    this.type = "game-object";
+    this.position = null;
+    this.velocity = new Vector(0, 0);
+    this.collisionBox = null;
+  }
+  update(dT) {
+    this.updatePosition(dT);
+  }
+  updatePosition(dT) {
+    var deltaPostion = Vector.scale(dT, this.velocity);
+    this.position = Vector.add(this.position, deltaPostion);
+  }
+  getCollisionBoxPosition() {
+    return this.collisionBox.addOffsetVector(this.position);
+  }
+  onCollision(collisionResult) {
+
+  }
+}
+
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  static add(vector1, vector2) {
+    var result = new Vector(0, 0);
+    result.x = vector1.x + vector2.x;
+    result.y = vector1.y + vector2.y;
+    return result;
+  }
+  static scale(scaleFactor, vector) {
+    return new Vector(scaleFactor * vector.x, scaleFactor * vector.y);
+  }
+  isInRectBounds(rect) {
+    return (
+      this.x > rect.x && this.x < rect.x + rect.width &&
+      this.y > rect.y && this.y < rect.y + rect.height
+    );
+  }
+}
+
+class Rect {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+  intersectsWith(otherRect) {
+    return (
+      this.x < otherRect.x + otherRect.width &&
+      this.x + this.width > otherRect.x &&
+      this.y < otherRect.y + otherRect.height &&
+      this.y + this.height > otherRect.y
+    );
+  }
+  calcIntersectionWith(otherRect) {
+    if (this.intersectsWith(otherRect)) {
+      var x1 = (this.x > otherRect.x) ? this.x : otherRect.x;
+      var x2 = (this.x + this.width < otherRect.x + otherRect.width) ? this.x + this.width : otherRect.x + otherRect.width;
+      var y1 = (this.y > otherRect.y) ? this.y : otherRect.y;
+      var y2 = (this.y + this.height < otherRect.y + otherRect.height) ? this.y + this.height : otherRect.y + otherRect.height;
+      return new Rect(x1, y1, x2 - x1, y2 - y1);
+    }
+    else {
+      return null;
+    }
+    var x = (this.x + this.width) - other.x
+  }
+  addOffsetVector(offsetVector) {
+    return new Rect(this.x + offsetVector.x, this.y + offsetVector.y, this.width, this.height);
+  }
+}
+
+class CollisionResult {
+  constructor(collideTarget, intersection) {
+    this.collideTarget = collideTarget;
+    this.intersection = intersection;
+  }
+}
--- js/Game/Game.js
+++ js/Game/Game.js
@@ -1,16 +1,7 @@
-class Game {
-  constructor() {
-    this.gameObjects = [];
-    this.tickNumber = 0;
-    this.isRunning = false;
-    this.viewport = new Viewport(this.gameObjects);
+class TestGame extends Game {
+  constructor(canvas) {
+    super(canvas);
     this.player = null;
-  }
-  // TODO: fix hacked-in player
-  start() {
-    this.isRunning = true;
-    this.initObjects();
-    this.main();
   }
   initObjects() {
     this.player = new Player();
@@ -22,87 +13,6 @@ class Game {
     obj1.position = new Vector(200, 200);
     obj1.collisionBox = new Rect(0, 0, 100, 100);
     this.gameObjects.push(obj1);
-  }
-  main() {
-    var currentTickTime = new Date().getTime();
-    var prevTickTime = currentTickTime;
-    var dT = 0;
-    setInterval(function() {
-      currentTickTime = new Date().getTime();
-      this.updateGameObjects(currentTickTime - prevTickTime);
-      this.checkCollisions();
-      prevTickTime = currentTickTime;
-    }.bind(this), 10);
-  }
-  updateGameObjects(dT) {
-    if (this.gameObjects.length > 0) {
-      this.gameObjects.forEach(function(gameObject) {
-        gameObject.update(dT);
-      });
-    }
-  }
-  // TODO: be more efficient
-  checkCollisions() {
-    for (var i = 0; i < this.gameObjects.length; i++) {
-      for (var j = i + 1; j < this.gameObjects.length; j++) {
-        var collisionOverlap = this.gameObjects[i].getOffsetCollisionBox().calcIntersectionWith(this.gameObjects[j].getOffsetCollisionBox());
-        if(collisionOverlap != null) {
-          this.gameObjects[i].onCollision(new CollisionResult(this.gameObjects[j], collisionOverlap));
-          this.gameObjects[j].onCollision(new CollisionResult(this.gameObjects[i], collisionOverlap));
-        }
-      }
-    }
-  }
-}
-
-class Viewport {
-  constructor(gameObjects) {
-    this.gameObjects = gameObjects;
-    this.draw();
-  }
-  draw() {
-    var canvas = document.getElementById("game-window");
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.save();
-
-      // draw all gameobjects based on collisionBox
-      this.gameObjects.forEach(function(gameObject) {
-        if(gameObject.type === "player") {
-          ctx.fillStyle = 'rgb(200, 0, 0)';
-        }
-        else {
-          ctx.fillStyle = 'rgb(0,200,0)';
-        }
-        ctx.fillRect(gameObject.position.x, gameObject.position.y, gameObject.collisionBox.width, gameObject.collisionBox.height);
-
-      }.bind(this));
-      ctx.restore();
-      window.requestAnimationFrame(this.draw.bind(this));
-    }
-  }
-}
-
-class GameObject {
-  constructor() {
-    this.type = "game-object";
-    this.position = null;
-    this.velocity = new Vector(0, 0);
-    this.collisionBox = null;
-  }
-  update(dT) {
-    this.updatePosition(dT);
-  }
-  updatePosition(dT) {
-    var deltaPostion = Vector.scale(dT, this.velocity);
-    this.position = Vector.add(this.position, deltaPostion);
-  }
-  getOffsetCollisionBox() {
-    return this.collisionBox.addOffsetVector(this.position);
-  }
-  onCollision(collisionResult) {
-
   }
 }
 
@@ -124,22 +34,23 @@ class Player extends GameObject {
   }
   onCollision(collisionResult) {
     if (collisionResult.intersection.width < collisionResult.intersection.height) {
-      if (this.getOffsetCollisionBox().x + this.getOffsetCollisionBox().width < collisionResult.collideTarget.getOffsetCollisionBox().x + collisionResult.collideTarget.getOffsetCollisionBox().width) {
+      if (this.getCollisionBoxPosition().x + this.collisionBox.width < collisionResult.collideTarget.getCollisionBoxPosition().x + collisionResult.collideTarget.collisionBox.width) {
         this.position.x -= collisionResult.intersection.width;
       }
-      else if (this.getOffsetCollisionBox().x > collisionResult.collideTarget.getOffsetCollisionBox().x) {
+      else if (this.getCollisionBoxPosition().x > collisionResult.collideTarget.getCollisionBoxPosition().x) {
         this.position.x += collisionResult.intersection.width;
       }
     }
     else {
-      if (this.getOffsetCollisionBox().y + this.getOffsetCollisionBox().height < collisionResult.collideTarget.getOffsetCollisionBox().y + collisionResult.collideTarget.getOffsetCollisionBox().height) {
+      if (this.getCollisionBoxPosition().y + this.collisionBox.height < collisionResult.collideTarget.getCollisionBoxPosition().y + collisionResult.collideTarget.collisionBox.height) {
         this.position.y -= collisionResult.intersection.height;
       }
-      else if (this.getOffsetCollisionBox().y > collisionResult.collideTarget.getOffsetCollisionBox().y) {
+      else if (this.getCollisionBoxPosition().y > collisionResult.collideTarget.getCollisionBoxPosition().y) {
         this.position.y += collisionResult.intersection.height;
       }
     }
   }
+
   getKeyInput() {
     var output = new Vector(0, 0);
     if (this.keyDown.left === true) {
@@ -164,70 +75,11 @@ class Player extends GameObject {
   }
 }
 
-class Vector {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-  static add(vector1, vector2) {
-    var result = new Vector(0, 0);
-    result.x = vector1.x + vector2.x;
-    result.y = vector1.y + vector2.y;
-    return result;
-  }
-  static scale(scaleFactor, vector) {
-    return new Vector(scaleFactor * vector.x, scaleFactor * vector.y);
-  }
-  isInRectBounds(rect) {
-    return (
-      this.x > rect.x && this.x < rect.x + rect.width &&
-      this.y > rect.y && this.y < rect.y + rect.height
-    );
-  }
-}
-
-class Rect {
-  constructor(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
-  intersectsWith(otherRect) {
-    return (
-      this.x < otherRect.x + otherRect.width &&
-      this.x + this.width > otherRect.x &&
-      this.y < otherRect.y + otherRect.height &&
-      this.y + this.height > otherRect.y
-    );
-  }
-  calcIntersectionWith(otherRect) {
-    if (this.intersectsWith(otherRect)) {
-      var x1 = (this.x > otherRect.x) ? this.x : otherRect.x;
-      var x2 = (this.x + this.width < otherRect.x + otherRect.width) ? this.x + this.width : otherRect.x + otherRect.width;
-      var y1 = (this.y > otherRect.y) ? this.y : otherRect.y;
-      var y2 = (this.y + this.height < otherRect.y + otherRect.height) ? this.y + this.height : otherRect.y + otherRect.height;
-      return new Rect(x1, y1, x2 - x1, y2 - y1);
-    }
-    else {
-      return null;
-    }
-    var x = (this.x + this.width) - other.x
-  }
-  addOffsetVector(offsetVector) {
-    return new Rect(this.x + offsetVector.x, this.y + offsetVector.y, this.width, this.height);
-  }
-}
-
-class CollisionResult {
-  constructor(collideTarget, intersection) {
-    this.collideTarget = collideTarget;
-    this.intersection = intersection;
-  }
-}
-
 $(document).ready(function() {
-  var game = new Game();
+  var canvas = document.getElementById("game-window");
+  var game = new TestGame(canvas);
+  game.initObjects();
+  game.viewport.drawDebugInfo = true;
   game.start();
 
   document.onkeydown = function(event) {
