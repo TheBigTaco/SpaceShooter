c1148a15e8e4246e29c3ae15fc559bc5948505ac
Brandon Kudo and Chris Turina <brandon+chris@>
2017-11-07 10:51:10 -0800

    add styling to profile page

--- SpaceShooter/wwwroot/js/scripts.js
+++ SpaceShooter/wwwroot/js/scripts.js
@@ -8,4 +8,32 @@ $(document).ready(function() {
     $(".menu-register").toggle();
     $(".search-form").fadeToggle();
   });
+
+  $("#sprite-button").click(function() {
+    $(".profile-sprites-hide").show();
+    $(".sprites-prompt").show();
+    $(".themes-prompt").hide();
+    $(".scores-prompt").hide();
+    $(".profile-themes-hide").hide();
+    $(".profile-scores-hide").hide();
+  });
+
+  $("#theme-button").click(function() {
+    $(".profile-themes-hide").show();
+    $(".themes-prompt").show();
+    $(".scores-prompt").hide();
+    $(".sprites-prompt").hide();
+    $(".profile-sprites-hide").hide();
+    $(".profile-scores-hide").hide();
+  });
+
+  $("#score-button").click(function() {
+    $(".profile-scores-hide").show();
+    $(".scores-prompt").show();
+    $(".themes-prompt").hide();
+    $(".sprites-prompt").hide();
+    $(".profile-sprites-hide").hide();
+    $(".profile-themes-hide").hide();
+  });
+
 });
